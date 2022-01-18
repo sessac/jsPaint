c840396bc5d42b589f9a77bebac4639c3a6c3c9c
sessac <96456334+sessac@users.noreply.github.com>
2022-01-18 17:54:19 +0900

    paint

---
+++ app.js
@@ -0,0 +1,101 @@
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColors");
+const range = document.querySelector("#jsRange");
+const newButton = document.querySelector("#jsNew");
+const modeButton = document.querySelector("#jsMode");
+const saveButton = document.querySelector("#jsSave");
+
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "#2c2c2c";
+ctx.lineWidth = 3;
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPaintion() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPaintion);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseout", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+function changeRange(event) {
+  const currentRange = event.target.value;
+  ctx.lineWidth = currentRange;
+}
+
+if (range) {
+  range.addEventListener("input", changeRange);
+}
+
+function handleNewButton() {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+newButton.addEventListener("click", handleNewButton);
+
+function handleModeButton() {
+  if (filling === true) {
+    filling = false;
+    modeButton.innerHTML = "Fill";
+  } else {
+    filling = true;
+    modeButton.innerHTML = "Paint";
+  }
+}
+
+function handleSaveButton() {}
+
+if (modeButton) {
+  modeButton.addEventListener("click", handleModeButton);
+}
+
+if (saveButton) {
+  saveButton.addEventListener("click", handleSaveButton);
+}
